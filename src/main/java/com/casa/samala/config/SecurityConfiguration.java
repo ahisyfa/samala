9233745e96826198d3b250f0e1f30cb16fb63e48
Ahmad Isyfalana Amin <ahmad.amin@dana.id>
2024-01-04 19:24:17 +0700

    SecurityConfiguration - Remove InMemoryUserDetailsManager and change PasswordEncoder type to BCryptPasswordEncoder

--- src/main/java/com/casa/samala/config/SecurityConfiguration.java
+++ src/main/java/com/casa/samala/config/SecurityConfiguration.java
@@ -9,11 +9,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
@@ -61,18 +58,8 @@ public class SecurityConfiguration {
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager() {
-        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
-        UserDetails userDetails = User.withUsername("admin").password("123456").authorities("READ").build();
-
-        inMemoryUserDetailsManager.createUser(userDetails);
-
-        return inMemoryUserDetailsManager;
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
     }
 
 }
