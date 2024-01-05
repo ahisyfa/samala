14a871558804caa6c42bb82857a0464f4c743e0d
Ahmad Isyfalana Amin <ahmad.amin@dana.id>
2024-01-06 00:04:15 +0700

    EnableJpaAuditing

--- src/main/java/com/casa/samala/SamalaApplication.java
+++ src/main/java/com/casa/samala/SamalaApplication.java
@@ -2,8 +2,10 @@ package com.casa.samala;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SamalaApplication {
 
 	public static void main(String[] args) {
