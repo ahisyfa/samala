43923f46611f55377196900f0d64ceb60e4cbf19
Ahmad Isyfalana Amin <ahmad.amin@dana.id>
2024-01-07 15:37:15 +0700

    Add placeholder Swagger Config

---
+++ src/main/java/com/casa/samala/config/SamalaSwaggerConfig.java
@@ -0,0 +1,11 @@
+package com.casa.samala.config;
+
+/**
+ *
+ *
+ * Please See: https://springdoc.org/#how-can-i-make-spring-security-login-endpoint-visible
+ * @author Ahmad Isyfalana Amin
+ * @version $Id: SamalaSwaggerConfig.java, v 0.1 2024-01-07  15.27 Ahmad Isyfalana Amin Exp $
+ */
+public class SamalaSwaggerConfig {
+}
