552f2b14c891c3ac1bc80a09f05faf90ba6e45ac
Ahmad Isyfalana Amin <ahmad.amin@dana.id>
2024-01-07 15:39:01 +0700

    Enable EnableConfigurationProperties

--- src/main/java/com/casa/samala/SamalaApplication.java
+++ src/main/java/com/casa/samala/SamalaApplication.java
@@ -1,11 +1,14 @@
 package com.casa.samala;
 
+import com.casa.samala.config.SamalaConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableConfigurationProperties(SamalaConfigProperties.class)
 public class SamalaApplication {
 
 	public static void main(String[] args) {
