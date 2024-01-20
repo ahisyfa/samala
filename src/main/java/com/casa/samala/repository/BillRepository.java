db258e6b328e54d1b951ec5c203f326f3101d3e5
Ahmad Isyfalana Amin <ahmad.amin@dana.id>
2024-01-20 18:49:54 +0700

    Modify repository

--- src/main/java/com/casa/samala/repository/BillRepository.java
+++ src/main/java/com/casa/samala/repository/BillRepository.java
@@ -1,8 +1,8 @@
 package com.casa.samala.repository;
 
 import com.casa.samala.entity.Bill;
-import com.casa.samala.entity.BillType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,7 +14,7 @@ import java.util.List;
  * @version $Id: BillRepository.java, v 0.1 2024-01-05  22.39 Ahmad Isyfalana Amin Exp $
  */
 @Repository
-public interface BillRepository extends JpaRepository<Bill, Long> {
+public interface BillRepository extends JpaRepository<Bill, Long>, JpaSpecificationExecutor<Bill> {
 
     @Override
     List<Bill> findAll();
